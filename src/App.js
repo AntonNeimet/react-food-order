4ccfd1ed16d6457411544e71b42b4971857bd088
Anton Neimet <neimet2204@gmail.com>
2023-03-26 12:35:48 +0300

    added card visibility

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
+import { useState } from 'react';
+
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
 import CartProvider from './store/CartProvider';
 
 function App() {
+  const [cardIsShown, setCardIsShown] = useState(false);
+
+  function showCardHandler() {
+    setCardIsShown(true);
+  }
+
+  function hideCardHandler() {
+    setCardIsShown(false)
+  }
   return (
     <CartProvider>
-      <Header />
+      {cardIsShown && <div>Card...</div>}
+      <Header onShowCard={showCardHandler} />
       <main>
         <Meals />
       </main>
-    </CartProvider>
+    </CartProvider> 
   );
 }
 
